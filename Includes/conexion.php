2b2ae2a77a5b5a32dcf60b45f557a92a92ebfd81
Jesusnm4 <a00813111@itesm.mx>
2015-04-21 19:39:54 -0500

    Login

--- Includes/conexion.php
+++ Includes/conexion.php
@@ -20,7 +20,7 @@ mysql_set_charset('utf8', $conexion);
 if (!$conexion) {
 	die('No pudo conectarse: ' . mysql_error());
 }
-$conexion_base = mysql_select_db('natacion', $conexion);
+$conexion_base = mysql_select_db('afp', $conexion);
 if (!$conexion_base) {
 	die('No se encuentra la base de datos seleccionada : ' . mysql_error());
 }

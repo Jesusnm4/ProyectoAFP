e6e9e8181b45e4aea2fe35214cb0c9bbaff23cf4
Jesusnm4 <a00813111@itesm.mx>
2015-04-23 03:32:43 -0500

    RegistroDiario.php

--- registroDiario.php
+++ registroDiario.php
@@ -4,20 +4,40 @@ include "./includes/conexion.php";
 include "./includes/sesionEstudiante.php";
 
 
-$date= $_POST["date"];
-$matricula= $_POST["matricula"];
 $tipo= $_POST["Tipo"];
-$series= $_POST["series"];
-$distancia= $_POST["distancia"];
-$tiempo= $_POST["tiempo"];
-$puls1= $_POST["puls1"];
-$puls3= $_POST["puls3"];
-$BORG= $_POST["BORG"];
-
-
-        $insert= "INSERT INTO registro_diario ( Matricula, Fecha, Num_Series, Distancia, Tiempo, Pulsacion_1, Pulsacion_3, Borg, Tipo)
+$insert="";
+if($tipo=="C"){
+    $date= $_POST["date"];
+    $matricula= $_POST["matricula"];
+    $distancia= $_POST["distancia"];
+    $tiempo= $_POST["tiempo"];
+    $puls1= $_POST["puls1"];
+    $puls3= $_POST["puls3"];
+    $BORG= $_POST["BORG"];
+    $insert= "INSERT INTO registro_diario ( Matricula, Fecha, Distancia, Tiempo, Pulsacion_1, Pulsacion_3, Borg, Tipo)
+            VALUES
+            ('$matricula', '$date', $distancia, '$tiempo', $puls1, $puls3, $BORG, '$tipo') ";
+}elseif($tipo=="M"){
+    $date= $_POST["date"];
+    $matricula= $_POST["matricula"];
+    $series= $_POST["series"];
+    $distancia= $_POST["distancia"];
+    $tiempo= $_POST["tiempo"];
+    $puls1= $_POST["puls1"];
+    $puls3= $_POST["puls3"];
+    $BORG= $_POST["BORG"];
+    $insert= "INSERT INTO registro_diario ( Matricula, Fecha, Num_Series, Distancia, Tiempo, Pulsacion_1, Pulsacion_3, Borg, Tipo)
             VALUES
             ('$matricula', '$date', $series, $distancia, '$tiempo', $puls1, $puls3, $BORG, '$tipo') ";
+}elseif($tipo=="F"){
+    $date= $_POST["date"];
+    $matricula= $_POST["matricula"];
+    $series= $_POST["series"];
+    $BORG= $_POST["BORG"];
+    $insert= "INSERT INTO registro_diario ( Matricula, Fecha, Num_Series, Borg, Tipo)
+            VALUES
+            ('$matricula', '$date', $series, $BORG, '$tipo') ";
+}
 
 		mysql_real_escape_string($insert);
 

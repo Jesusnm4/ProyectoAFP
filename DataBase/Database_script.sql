050d99521fcf2ad39163799afb50e203704c4679
Jesusnm4 <a00813111@itesm.mx>
2015-04-23 01:47:21 -0500

    DB

--- DataBase/Database_script.sql
+++ DataBase/Database_script.sql
@@ -78,6 +78,11 @@ INSERT INTO `usuario` (`Nomina`, `Nombre`, `Password`, `Tipo`) VALUES
 INSERT INTO `usuario` (`Nomina`, `Nombre`, `Password`, `Tipo`) VALUES
 ('PROFE', 'Profesor', 'profeAFP', 'P');
 
-INSERT INTO `usuario` (`Nomina`, `Nombre`, `Password`, `Tipo`,`Profesor`,`Puede_Cita`) VALUES
-('ESTUDIANTE', 'Estudiante', 'estudianteAFP', 'E', 'PROFE', 0);
+INSERT INTO `usuario` (`Nomina`, `Nombre`, `Password`, `Tipo`,`Profesor`,`Puede_Cita`,`Cita_Disponible`) VALUES
+('ESTUDIANTE', 'Estudiante', 'estudianteAFP', 'E', 'PROFE', 0, 1);
+
+CREATE EVENT AutoDeleteOldNotifications
+ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 14 DAY
+DO
+DELETE LOW_PRIORITY FROM afp.agenda WHERE Fecha < DATE_SUB(NOW(), INTERVAL 14 DAY)
 
